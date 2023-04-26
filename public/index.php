62d96c931424e9601d8c1e9cc034b508d6921db8
Robson Souza Lourenço <robson.lourenco.pacheco@gmail.com>
2023-04-26 15:30:21 -0300

    Atualizando com o projeto original.

--- public/index.php
+++ public/index.php
@@ -3,6 +3,7 @@
 use Alura\CursoMvc\Controller\VideoController;
 use Alura\CursoMvc\Controller\AllVideosController;
 use Alura\CursoMvc\Entity\Video;
+use Nyholm\Psr7\Factory\Psr17Factory;
 
 require_once '../vendor/autoload.php';
 $routes = require_once __DIR__ . "/../config/routes.php";
@@ -18,11 +19,15 @@ if (!array_key_exists($key, $routes)) {
     throw new DomainException("Rota não encontrada");
 }
 
+$psr17Factory = new Psr17Factory();
+$request = $psr17Factory->createRequest($httpMethod, $pathInfo);
+
 $controllerClass = $routes["$httpMethod|$pathInfo"];
 
 $controller = new $controllerClass();
-$controller->processaRequisicao();
+$response = $controller->processaRequisicao($request);
 
+// var_dump($response); exit();
 $isLoginRoute = $pathInfo === '/login';
 if (!array_key_exists('logado', $_SESSION) && !$isLoginRoute) {
     header('location: /login');
